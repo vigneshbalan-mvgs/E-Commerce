d85114c94c1be7dd8a5b36192092f75f47d1a010
MVGS <vigneshbalanmvgs2003@gmail.com>
2023-10-26 09:22:35 +0530

    1.1

--- src/Components/Items/Item.jsx
+++ src/Components/Items/Item.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import './item.css'
 
 
-const Item = () => {
+const Item = (props) => {
   return (
-    <div className='item'></div>
+    <div className='item'>
+      <img src="{props.image}" alt="" />
+      <p>{props.name}</p>
+      <div className="item-prices">
+         <div className="item-price-new">
+          {props.new_price}
+         </div>
+         <div className="item-price-old">
+          {props.old_price}
+         </div>
+      </div>
+    </div>
   )
 }
 
