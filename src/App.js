0a858b5813a78e7d91605aea3ee273afb20fc1da
vignesh mvgs <vigneshbalanmvgs2003@gmail.com>
2024-06-03 19:01:37 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import Navbar from './Components/Navbar/Navbar'
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Prodcut';
 import Cart from './Pages/Cart';
-import LoginSignup  from './Pages/LoginSignup';
+import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
 import men_banner from './Components/Assets/banner_mens.png';
 import women_banner from './Components/Assets/banner_women.png';
@@ -17,19 +17,19 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Shop />}></Route>
-        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}></Route>
-        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}></Route>
-        <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid"/>}></Route>
-        <Route path='/produt' element={<Product/>}>
-          <Route path=':productId' element={<Product/>}></Route>
-        </Route>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/login' element={<LoginSignup />}/>
-      </Routes>
-      <Footer />
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />}></Route>
+          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />}></Route>
+          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />}></Route>
+          <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid" />}></Route>
+          <Route path='/product' element={<Product />}>
+            <Route path=':productId' element={<Product />}></Route>
+          </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
