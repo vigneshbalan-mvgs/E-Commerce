13656de32077aca387460fe62af54d6b6a78ded1
vignesh mvgs <vigneshbalanmvgs2003@gmail.com>
2024-06-03 18:44:02 +0530

    updated

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Cart = () => {
   return (
-    <div></div>
-  )
-}
+    <div>
+      Under Developing
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
+
